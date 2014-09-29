2cedcf103022913038438bfea8e8994ce9df8f02
Arpit Jain <arpitjain@arpits-mbp.home>
2014-09-29 00:34:59 -0400

    cosine, phrase, numview

--- src/edu/nyu/cs/cs2580/Ranker.java
+++ src/edu/nyu/cs/cs2580/Ranker.java
@@ -46,183 +46,165 @@ class Ranker {
         }
       }
     }
-
     return new ScoredDocument(did, d.get_title_string(), score);
   }
   
   public Vector < ScoredDocument > runquery_cosine(String query){
 	  
-	    Vector < ScoredDocument > retrieval_results = new Vector < ScoredDocument > ();
-	    for (int i = 0; i < _index.numDocs(); ++i){
-	      retrieval_results.add(runquery_cosine(query, i));
-	    }
-	    return retrieval_results;
-	  }
+    Vector < ScoredDocument > retrieval_results = new Vector < ScoredDocument > ();
+    for (int i = 0; i < _index.numDocs(); ++i){
+      retrieval_results.add(runquery_cosine(query, i));
+    }
+    return retrieval_results;  
+  }
 
-	  public ScoredDocument runquery_cosine(String query, int did){
+  public ScoredDocument runquery_cosine(String query, int did){
 
-	    // Build query vector
-	    Scanner s = new Scanner(query);
-	    Vector < String > qv = new Vector < String > ();
-	    while (s.hasNext()){
-	      String term = s.next();
-	      qv.add(term);
-	    }
+    // Build query vector
+    Scanner s = new Scanner(query);
+    Vector < String > qv = new Vector < String > ();
+    while (s.hasNext()){
+      String term = s.next();
+      qv.add(term);
+    }
 
-	    // Get the document vector. For hw1, you don't have to worry about the
-	    // details of how index works.
-	    Document d = _index.getDoc(did);
-	    Vector < String > dv = d.get_body_vector();
-	    
+    Document d = _index.getDoc(did);
+    Vector < String > dv = d.get_body_vector();
+    
 
-	    // Score the document. Here we have provided a very simple ranking model,
-	    // where a document is scored 1.0 if it gets hit by at least one query term.
-	    
-	    
-	    
-	    double score = 0.0;
-	    Integer idx=0;
-	    
-	      for (int j = 0; j < qv.size(); ++j){
-<<<<<<< HEAD
-	       
-	    	  
-=======
-	        	
->>>>>>> FETCH_HEAD
-	          score += d.get_term_tfidf(qv.get(j));
-	          
-	      }
-	      
-	      
-	      score=score*((double)1/Math.sqrt(qv.size()));
-	    
+    // Score the document. Here we have provided a very simple ranking model,
+    // where a document is scored 1.0 if it gets hit by at least one query term.
+    
+    
+    
+    double score = 0.0;
+    Integer idx=0;
+    
+      for (int j = 0; j < qv.size(); ++j){
 
-	    return new ScoredDocument(did, d.get_title_string(), score);
-	  }
+          score += d.get_term_tfidf(qv.get(j));
+          
+      }
+      
+      
+      score=score*((double)1/Math.sqrt(qv.size()));
+    
+
+    return new ScoredDocument(did, d.get_title_string(), score);
+  }
 	  
 	  
   
-	  public Vector < ScoredDocument > runquery_phrase(String query){
+  public Vector < ScoredDocument > runquery_phrase(String query){
+	  
+    Vector < ScoredDocument > retrieval_results = new Vector < ScoredDocument > ();
+    for (int i = 0; i < _index.numDocs(); ++i){
+      retrieval_results.add(runquery_phrase(query, i));
+    }
+    return retrieval_results;
+  }
+
+  public ScoredDocument runquery_phrase(String query, int did){
+
+    // Build query vector
+    Scanner s = new Scanner(query);
+    Vector < String > qv = new Vector < String > ();
+    while (s.hasNext()){
+      String term = s.next();
+      qv.add(term);
+    }
+
+    // Get the document vector. For hw1, you don't have to worry about the
+    // details of how index works.
+    Document d = _index.getDoc(did);
+    Vector < String > dv = d.get_body_vector();
+    
+
+    // Score the document. Here we have provided a very simple ranking model,
+    // where a document is scored 1.0 if it gets hit by at least one query term.
+    
+    
+    
+    double score = 0.0;
+    Integer idx=0;
+    
+    for (int i = 0; i < dv.size()-1; ++i){
+        for (int j = 0; j < qv.size()-1; ++j){
+          if ( (dv.get(i).equals(qv.get(j)) && (dv.get(i+1).equals(qv.get(j+1))) )){
+            score += 1.0;
+            break;
+          }
+        }
+     }
+    
+     
+
+    return new ScoredDocument(did, d.get_title_string(), score);
+  }
 		  
-		    Vector < ScoredDocument > retrieval_results = new Vector < ScoredDocument > ();
-		    for (int i = 0; i < _index.numDocs(); ++i){
-		      retrieval_results.add(runquery_phrase(query, i));
-		    }
-		    return retrieval_results;
-		  }
+  public Vector < ScoredDocument > runquery_numviews(String query){
+	  
+	    Vector < ScoredDocument > retrieval_results = new Vector < ScoredDocument > ();
+	    for (int i = 0; i < _index.numDocs(); ++i){
+	      retrieval_results.add(runquery_numviews(query, i));
+	    }
+	    return retrieval_results;
+  }
 
-		  public ScoredDocument runquery_phrase(String query, int did){
+  public ScoredDocument runquery_numviews(String query, int did){
 
-		    // Build query vector
-		    Scanner s = new Scanner(query);
-		    Vector < String > qv = new Vector < String > ();
-		    while (s.hasNext()){
-		      String term = s.next();
-		      qv.add(term);
-		    }
+    // Build query vector
+    Scanner s = new Scanner(query);
+    Vector < String > qv = new Vector < String > ();
+    while (s.hasNext()){
+      String term = s.next();
+      qv.add(term);
+    }
 
-		    // Get the document vector. For hw1, you don't have to worry about the
-		    // details of how index works.
-		    Document d = _index.getDoc(did);
-		    Vector < String > dv = d.get_body_vector();
-		    
+    // Get the document vector. For hw1, you don't have to worry about the
+    // details of how index works.
+    Document d = _index.getDoc(did);
+    Vector < String > dv = d.get_body_vector();
+    
 
-		    // Score the document. Here we have provided a very simple ranking model,
-		    // where a document is scored 1.0 if it gets hit by at least one query term.
-		    
-		    
-		    
-		    double score = 0.0;
-		    Integer idx=0;
-		    
-		    for (int i = 0; i < dv.size()-1; ++i){
-		        for (int j = 0; j < qv.size()-1; ++j){
-		          if ( (dv.get(i).equals(qv.get(j)) && (dv.get(i+1).equals(qv.get(j+1))) )){
-		            score += 1.0;
-		            break;
-		          }
-		        }
-		     }
-		    
-		     
+    // Score the document. Here we have provided a very simple ranking model,
+    // where a document is scored 1.0 if it gets hit by at least one query term.
+        
+    double score = d.get_numviews();
 
-		    return new ScoredDocument(did, d.get_title_string(), score);
-		  }
-		  
-		  public Vector < ScoredDocument > runquery_numviews(String query){
+    return new ScoredDocument(did, d.get_title_string(), score);
+  }	
 			  
-			    Vector < ScoredDocument > retrieval_results = new Vector < ScoredDocument > ();
-			    for (int i = 0; i < _index.numDocs(); ++i){
-			      retrieval_results.add(runquery_numviews(query, i));
-			    }
-			    return retrieval_results;
-			  }
+  public Vector < ScoredDocument > runquery_linear(String query){
+	  
+	Vector < ScoredDocument > retrieval_results = new Vector < ScoredDocument > ();
+	
+	Vector < ScoredDocument > cosine_results = new Vector < ScoredDocument > ();
+	Vector < ScoredDocument > lm_results = new Vector < ScoredDocument > ();
+	Vector < ScoredDocument > phrase_results = new Vector < ScoredDocument > ();
+	Vector < ScoredDocument > numviews_results = new Vector < ScoredDocument > ();
+	
+	cosine_results=runquery_cosine(query);
+	phrase_results=runquery_phrase(query);
+	//lm_results=runquery_lm(query);
+	numviews_results=runquery_numviews(query);
+    
+	Double b_c=1.0;
+	Double b_p=1.0;
+	Double b_n=1.0; 
+	    
+	Double score=0.0;
+	
+    for (int i = 0; i < _index.numDocs(); ++i){
+    	
+    score = b_c*(cosine_results.get(i)._score)+b_p*(phrase_results.get(i)._score)+b_n*(numviews_results.get(i)._score);
 
-			  public ScoredDocument runquery_numviews(String query, int did){
+    retrieval_results.add(new ScoredDocument(i, cosine_results.get(i)._title, score));
+	    
+    } 
 
-			    // Build query vector
-			    Scanner s = new Scanner(query);
-			    Vector < String > qv = new Vector < String > ();
-			    while (s.hasNext()){
-			      String term = s.next();
-			      qv.add(term);
-			    }
-
-			    // Get the document vector. For hw1, you don't have to worry about the
-			    // details of how index works.
-			    Document d = _index.getDoc(did);
-			    Vector < String > dv = d.get_body_vector();
-			    
-
-			    // Score the document. Here we have provided a very simple ranking model,
-			    // where a document is scored 1.0 if it gets hit by at least one query term.
-			    
-			    
-			    
-			    double score = d.get_numviews();
-			    
-			    
-			    
-			    
-			     
-
-			    return new ScoredDocument(did, d.get_title_string(), score);
-			  }	
-			  
-			  public Vector < ScoredDocument > runquery_linear(String query){
-				  
-				    Vector < ScoredDocument > retrieval_results = new Vector < ScoredDocument > ();
-				    
-				    Vector < ScoredDocument > cosine_results = new Vector < ScoredDocument > ();
-				    Vector < ScoredDocument > lm_results = new Vector < ScoredDocument > ();
-				    Vector < ScoredDocument > phrase_results = new Vector < ScoredDocument > ();
-				    Vector < ScoredDocument > numviews_results = new Vector < ScoredDocument > ();
-				    
-				    cosine_results=runquery_cosine(query);
-				    phrase_results=runquery_phrase(query);
-				    //lm_results=runquery_lm(query);
-				    numviews_results=runquery_numviews(query);
-				    
-				   Double b_c=1.0;
-				   Double b_p=1.0;
-				   Double b_n=1.0; 
-				    
-				   Double score=0.0;
-				    for (int i = 0; i < _index.numDocs(); ++i){
-				    	
-				    score = b_c*(cosine_results.get(i)._score)+b_p*(phrase_results.get(i)._score)+b_n*(numviews_results.get(i)._score);
-				    
-				   
-				    
-				    retrieval_results.add(new ScoredDocument(i, cosine_results.get(i)._title, score));
-					    
-				    } 
-				    	
-				    	
-				    
-				    return retrieval_results;
-				  }
+    return retrieval_results;
+  }
 
 				  	  
 			  
