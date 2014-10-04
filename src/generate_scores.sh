a2f4d6e2f2baad73c8372568aa6efe3893debc11
Arpit Jain <arpitjain91@gmail.com>
2014-10-04 04:49:51 -0400

    Resolved lib curl http not supported

--- src/generate_scores.sh
+++ src/generate_scores.sh
@@ -44,7 +44,7 @@ do
 	    echo "Creating file... $FILE"
 	    touch $FILE
 	fi
-	curl " http://linserv1.nyu.edu:25808/search?query=$QUERY&ranker=$RANKER&format=text" >> $FILE
+	curl "http://localhost:25808/search?query=$QUERY&ranker=$RANKER&format=text" >> $FILE
 
 
 	QUESTION=/hw1.3-
@@ -54,7 +54,7 @@ do
 	    echo "Creating file... "$FILE
 	    touch $FILE
 	fi
-	curl " http://linserv1.nyu.edu:25808/search?query="$QUERY"&ranker="$RANKER"&format=text" | java edu.nyu.cs.cs2580.Evaluator $JUDGEMENTS >> $FILE
+	curl "http://localhost:25808/search?query="$QUERY"&ranker="$RANKER"&format=text" | java edu.nyu.cs.cs2580.Evaluator $JUDGEMENTS >> $FILE
 
     done
 done
